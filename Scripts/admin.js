d162cfd8f20532bf002e23bccda24d739f3bfc5c
Niharika Pandey <niharikapandeyabc@gmail.com>
2023-01-19 02:49:56 +0530

    day-2

---
+++ Scripts/admin.js
@@ -0,0 +1,38 @@
+
+let form=document.querySelector("#form");
+let Category = document.querySelector("#filter")
+let product= document.querySelector("#product"); 
+let img =document.querySelector("#img")  
+let desc=document.querySelector("#desc")
+let checkbox1 = document.querySelector("#a1").value;
+let checkbox2 = document.querySelector("#a2").value; 
+let checkbox3 = document.querySelector("#a3").value;
+let checkbox4 = document.querySelector("#a4").value;
+let desc1=document.querySelector("#desc1").value;
+let desc2=document.querySelector("#desc2").value;
+let desc3=document.querySelector("#desc3").value;
+let desc4=document.querySelector("#desc4").value;
+form.addEventListener("submit",function(events){
+   events.preventDefault();
+console.log("x")
+ 
+  
+})
+  
+ 
+ 
+
+ 
+    let added=document.querySelector("#added");
+    
+added.addEventListener("click",function(){
+    window.location.href="../Pages/adminpro.html"
+    
+})
+    
+  
+
+  
+ 
+
+   
