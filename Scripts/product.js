cbd4ff4a49baf2c9a1289ff0ed7395e25642eb3e
Sagar Rajwar <sagarraj16.sr@gmail.com>
2023-01-21 23:25:35 +0530

    day_4

--- Scripts/product.js
+++ Scripts/product.js
@@ -1,4 +1,19 @@
+// navbar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
 
+const loginBtn = document.getElementById("login-btn");
+const slidingLogin = document.getElementById("sliding-login");
+ 
+loginBtn.addEventListener("click", function() {
+  slidingLogin.classList.toggle("active");
+});
+// navbar done
 
 let container = document.getElementById("main-container");
 
