46fdc4912ccd4bba48842c0f3f96e95668dcb7f2
Divyansh <divyanshgoel777@gmail.com>
2023-01-22 21:21:37 +0530

    Login and signup

--- Scripts/login.js
+++ Scripts/login.js
@@ -4,6 +4,9 @@ const usersapi = "https://dull-gold-badger-suit.cyclic.app/users";
 
  let myForm = document.querySelector("form");
  
+     //Create logout button
+let logoutBtn = document.createElement("button");
+logoutBtn.innerText = "Logout";
 // var id = 2
  
  myForm.addEventListener("submit",(e)=>{
@@ -12,15 +15,6 @@ const usersapi = "https://dull-gold-badger-suit.cyclic.app/users";
 
      let OTP = Math.floor(1000+Math.random() * 9000);
 
-
-    // let userObj = {
-    //     "id":id,
-    //     "phone":myForm.phone.value,
-    //     "address":null,
-    //     "otp":OTP,
-    // }
-    
-    
 
     fetch(usersapi)
     .then((res) => res.json())
@@ -39,12 +33,19 @@ const usersapi = "https://dull-gold-badger-suit.cyclic.app/users";
         for(let i=0;i<=data.length-1;i++){
             if(data[i].phone==myForm.phone.value){
                dataOtpPatch(OTP,data[i].id)
+              // swal(`PLZ NOTE DOWN THE ${OTP}`,"","warning")
+                Checkotp(OTP)
+             
+
                 flag=false
                 break;
             }
         }
         if(flag==true){
             dataOTP(userObj)
+            Checkotp(OTP)
+        
+ 
             id++
             console.log(id)
         }
@@ -91,3 +92,92 @@ const usersapi = "https://dull-gold-badger-suit.cyclic.app/users";
 
 
 
+    function Checkotp(OTP){
+        swal({
+            title: 'OTP FOR LOGIN',
+            text: `OTP : ${OTP}`,
+            icon: 'info',
+            showCancelButton: false,
+            confirmButtonText: 'OK'
+          }).then((result) => {
+            if (result.value) {
+              // Step 2: Prompt for OTP
+              swal({
+                title: 'Enter OTP',
+                input: 'text',
+                inputAttributes: {
+                  autocapitalize: 'off'
+                },
+                showCancelButton: true,
+                confirmButtonText: 'Submit',
+                showLoaderOnConfirm: true,
+              }).then((result) => {
+                if (result.value) {
+                  if (result.value==OTP) {
+                  
+                    swal({
+                      title: 'Logged In successfully!',
+                      text: 'You are logged in successfully.',
+                      icon: 'success'
+                    });
+                    
+                    JSON.parse(localStorage.getItem("login")) || "Login";
+
+                    localStorage.setItem("login",JSON.stringify(myForm.phone.value))
+
+                    this.location.reload()
+
+                    let loginBtn = document.getElementById("login-btn");
+ 
+                   
+
+                  } else {
+
+           
+                    loginBtn.innerText='Login'
+        
+                   
+                   swal({
+
+                      title: 'Invalid OTP',
+                      text: 'Try Again',
+                      icon: 'error'
+                    });
+                  }
+                }
+              });
+            }
+          });
+    }
+
+
+    let getvalue =  JSON.parse(localStorage.getItem("login")) || "Login";
+
+    let loginbt = document.getElementById("login-btn")
+    
+    
+    loginbt.innerText= "Hi " + getvalue
+
+
+
+
+
+    loginbt.parentNode.appendChild(logoutBtn);
+
+    
+    logoutBtn.addEventListener("click", function() {
+        
+        loginbt.parentNode.appendChild(logoutBtn).style.display="none";;
+    swal({
+
+        title: 'Logout Successfully',
+        text: 'Thank you for visiting.',
+        icon: 'success'
+      })
+
+
+  localStorage.removeItem("login");
+  loginbt.innerText = "Login";
+});
+
+   
--- Scripts/product.js
+++ Scripts/product.js
@@ -1,131 +1,148 @@
 // navbar
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+    document.getElementById("mySide").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySide").style.width = "0";
+}
 
 const loginBtn = document.getElementById("login-btn");
 const slidingLogin = document.getElementById("sliding-login");
- 
-loginBtn.addEventListener("click", function() {
-  slidingLogin.classList.toggle("active");
+
+loginBtn.addEventListener("click", function () {
+    slidingLogin.classList.toggle("active");
 });
 // navbar done
 
 
 // "start": "json-server --watch ./JSON/db.json"
 
-let keys = ["bestseller","newlaunches","parathapizza","vegpizza","gourmet","nonvegpizza","beverages","specialitychicken","sides","pizzamania","lunchcombo","mealfortwo","mealforfour","dessert","chefboss"]
+let keys = ["bestseller", "newlaunches", "parathapizza", "vegpizza", "gourmet", "nonvegpizza", "beverages", "specialitychicken", "sides", "pizzamania", "lunchcombos", "mealfortwo", "mealforfour", "dessert", "chefboss"]
 
 let container = document.getElementById("main-container");
 
-for(let i=0;i<keys.length;i++){
+for (let i = 0; i < keys.length; i++) {
 
-fetch("http://localhost:3000/"+keys[i])
-.then((res)=>{
-return  res.json();
-})
-.then((data)=>{
+    fetch(`https://dull-gold-badger-suit.cyclic.app/${keys[i]}`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
 
-console.log(data)
-card(data,keys[i])
+            //console.log(data)
+            card(data, keys[i])
 
-})
-.catch((error)=>{
-console.log(error);
-})
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 
 }
 
-function card(data,key){
+function card(data, key) {
 
     //container.innerHTML=null;
-    
+
     let heading = document.createElement("h2");
 
-   heading.innerText = key.toUpperCase();
+    heading.innerText = key.toUpperCase();
 
-    heading.setAttribute("class","sec-head")
-    heading.setAttribute("id",key)
+    heading.setAttribute("class", "sec-head")
+    heading.setAttribute("id", key)
     container.append(heading);
 
     let main = document.createElement("div");
-    main.setAttribute("class","main")
+    main.setAttribute("class", "main")
 
-    data.forEach(function(element,index){
+    data.forEach(function (element, index) {
 
-        let des = element.desc ? element.desc.substring(0,125)+"..." : "No description"
+        let des = element.desc ? element.desc.substring(0, 125) + "..." : "No description"
 
         let one = document.createElement("div");
-        one.setAttribute("class","one")
+        one.setAttribute("class", "one")
 
         let image = document.createElement("img");
-        image.setAttribute("src",element.image);
-  
-        let name= document.createElement("h3");
-        name.innerText =  element.product;
-      
-      let btn = document.createElement("button");
-      btn.innerText = "ADD TO CART"
+        image.setAttribute("src", element.image);
 
-      let desc= document.createElement("p");
-        desc.innerText =  des;
+        let name = document.createElement("h3");
+        name.innerText = element.product;
+
+        let btn = document.createElement("button");
+        btn.innerText = "ADD TO CART"
+
+        let desc = document.createElement("p");
+        desc.innerText = des;
 
         let size = document.createElement("select");
-        
+
         let prices = document.createElement("select");
         let def = document.createElement("option");
-        def.innerText="Size"
+        def.innerText = "Size"
         size.append(def)
-        for(let size1 in element.size){
+        for (let size1 in element.size) {
             let selects = document.createElement("option");
-            selects.setAttribute("value",size1)
-            selects.innerText = size1 
+            selects.setAttribute("value", size1)
+            selects.innerText = size1
             // for(let price in element.size[size1]){
             //     let sel = document.createElement("option");
             //     sel.innerText = element.size[size1]
             //     prices.append(sel); 
             // }
-        size.append(selects);
+            size.append(selects);
         }
         prices.innerText = "Hello"
-        size.addEventListener("change",(e)=>{
-            
-            if(e.target.value=="Regular"){
-               prices.innerHTML = null;
-                for(let key in element.size.Regular){
+        size.addEventListener("change", (e) => {
+
+            if (e.target.value == "Regular") {
+                prices.innerHTML = null;
+                for (let key in element.size.Regular) {
                     let sel = document.createElement("option");
-                sel.innerText = key + element.size.Regular[key]
-                prices.append(sel); 
+
+                    sel.innerText = key + element.size.Regular[key]
+                    console.log(element.size.Regular[key])
+                    prices.append(sel);
                 }
             }
-            else if(e.target.value=="Medium"){
-             prices.innerHTML = null;
-                for(let key in element.size.Medium){
+            else if (e.target.value == "Medium") {
+                prices.innerHTML = null;
+                for (let key in element.size.Medium) {
                     let sel = document.createElement("option");
-                sel.textContent = key + element.size.Medium[key]
-                prices.append(sel); 
+
+                    if (element.size.Medium[key] == null) {
+                        sel.textContent = key;
+                        sel.disabled()
+                    }
+                    else {
+
+                        sel.textContent = `${key} ===> ${element.size.Medium[key]}`
+                    }
+                    prices.append(sel);
                 }
             }
-            else if(e.target.value=="Large"){
-               prices.innerHTML = null;
-                for(let key in element.size.Large){
+            else if (e.target.value == "Large") {
+                prices.innerHTML = null;
+                for (let key in element.size.Large) {
                     let sel = document.createElement("option");
-                sel.innerText = key + element.size.Large[key]
-                prices.append(sel); 
+                    if (element.size.Medium[key] == null) {
+                        sel.textContent = key;
+                        sel.disabled()
+                    }
+                    else {
+
+                        sel.textContent = `${key} ===> ${element.size.Large[key]}`
+                    }
+                    prices.append(sel);
                 }
             }
-            else if(e.target.value=="price"){
-               prices.innerHTML = null;
-                
+            else if (e.target.value == "price") {
+                prices.innerHTML = null;
+
                 let sel = document.createElement("option");
                 sel.innerText = element.size.price
-                prices.append(sel); 
-            
-            }else{
+                prices.append(sel);
+
+            } else {
                 prices.innerHTML = null
             }
 
@@ -144,7 +161,7 @@ function card(data,key){
 
         // let price = document.createElement("h4");
         // price.innerText = "MRP:"+ element.price;
-        one.append(image,name,desc,size,prices,btn);
+        one.append(image, name, desc, size, prices, btn);
         main.append(one)
         container.append(main);
     })
@@ -152,15 +169,88 @@ function card(data,key){
 
 }
 
-document.querySelector("a").addEventListener("click", function() {
-    document.getElementById("sides").scrollIntoView({behavior: 'smooth'});
-  });
+let bestseller = document.getElementById("bestseller")
+let newlaunches = document.getElementById("newlaunches")
+let sides = document.getElementById("sides")
+let parathapizza = document.getElementById("parathapizza")
+let vegpizza = document.getElementById("vegpizza")
+let nonvegpizza = document.getElementById("nonvegpizza")
+let beverages = document.getElementById("beverages")
+let pizzamania = document.getElementById("pizzamania")
+let lunchcombos = document.getElementById("lunchcombos")
+let mealfortwo = document.getElementById("mealfortwo")
+let mealforfour = document.getElementById("mealforfour")
+let dessert = document.getElementById("dessert")
+let chefboss = document.getElementById("chefboss")
+let specialitychicken = document.getElementById("specialitychicken")
 
 
 
 
 
+sides.addEventListener("click", function () {
+    sides.scrollIntoView({ behavior: 'smooth' });
+});
 
 
+bestseller.addEventListener("click", function () {
+    bestseller.scrollIntoView({ behavior: 'smooth' });
+});
+
+newlaunches.addEventListener("click", function () {
+    newlaunches.scrollIntoView({ behavior: 'smooth' });
+});
+
+parathapizza.addEventListener("click", function () {
+    parathapizza.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+vegpizza.addEventListener("click", function() {
+    vegpizza.scrollIntoView({behavior: 'smooth'});
+        });
+
+nonvegpizza.addEventListener("click", function() {
+    nonvegpizza.scrollIntoView({behavior: 'smooth'});
+});
+
+
+beverages.addEventListener("click", function() {
+    beverages.scrollIntoView({behavior: 'smooth'});
+});
+
+
+pizzamania.addEventListener("click", function() {
+    pizzamania.scrollIntoView({behavior: 'smooth'});
+});
+                
+
+lunchcombos.addEventListener("click", function() {
+    lunchcombos.scrollIntoView({behavior: 'smooth'});
+});
+
+mealfortwo.addEventListener("click", function() {
+    specialitychicken.scrollIntoView({behavior: 'smooth'});
+});
+
+
+mealforfour.addEventListener("click", function() {
+    mealforfour.scrollIntoView({behavior: 'smooth'});
+});
+
+
+dessert.addEventListener("click", function() {
+    dessert.scrollIntoView({behavior: 'smooth'});
+});
+
+
+chefboss.addEventListener("click", function() {
+    chefboss.scrollIntoView({behavior: 'smooth'});
+});
+
+
+specialitychicken.addEventListener("click", function() {
+    specialitychicken.scrollIntoView({behavior: 'smooth'});
+});
 
 
