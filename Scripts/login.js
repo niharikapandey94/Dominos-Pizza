a494ba69cb9db941880b0f60de265d78862e8aa4
Sagar Rajwar <sagarraj16.sr@gmail.com>
2023-01-23 06:08:48 +0530

    sliding

--- Scripts/login.js
+++ Scripts/login.js
@@ -2,184 +2,184 @@
 
 const usersapi = "https://dull-gold-badger-suit.cyclic.app/users";
 
- let myForm = document.querySelector("form");
- 
-     //Create logout button
+let myForm = document.querySelector("form");
+
+    //Create logout button
 let logoutBtn = document.createElement("button");
 logoutBtn.innerText = "Logout";
 // var id = 2
- 
- myForm.addEventListener("submit",(e)=>{
-     
-     e.preventDefault();
 
-     let OTP = Math.floor(1000+Math.random() * 9000);
+myForm.addEventListener("submit",(e)=>{
+    
+    e.preventDefault();
 
-     if(myForm.phone.value=="0000000000"){
-        window.location.replace("admin.html");
-    }else{
-    fetch(usersapi)
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        let flag = true
+    let OTP = Math.floor(1000+Math.random() * 9000);
 
-        let userObj = {
-            "id":data.length+1,
-            "phone":myForm.phone.value,
-            "address":null,
-            "otp":OTP,
-        }
+    if(myForm.phone.value=="0000000000"){
+       window.location.replace("admin.html");
+   }else{
+   fetch(usersapi)
+   .then((res) => res.json())
+   .then((data) => {
+       console.log(data)
+       let flag = true
 
-        
-        for(let i=0;i<=data.length-1;i++){
-            if(data[i].phone==myForm.phone.value){
-               dataOtpPatch(OTP,data[i].id)
-              // swal(`PLZ NOTE DOWN THE ${OTP}`,"","warning")
-                Checkotp(OTP)
-             
+       let userObj = {
+           "id":data.length+1,
+           "phone":myForm.phone.value,
+           "address":null,
+           "otp":OTP,
+       }
 
-                flag=false
-                break;
-            }
-        }
-        if(flag==true){
-            dataOTP(userObj)
-            Checkotp(OTP)
-        
- 
-            id++
-            console.log(id)
-        }
-      }); 
-    }
-    })
-    function dataOtpPatch(OTP,data1){
-        fetch(`${usersapi}/${data1}`,{
-            method:"PATCH",
-            headers:{
-                "Content-type":"application/json",
-            },
-            body:JSON.stringify({otp:OTP})
-        })
-        .then((res)=>{
+       
+       for(let i=0;i<=data.length-1;i++){
+           if(data[i].phone==myForm.phone.value){
+              dataOtpPatch(OTP,data[i].id)
+             // swal(`PLZ NOTE DOWN THE ${OTP}`,"","warning")
+               Checkotp(OTP)
+            
 
-            // if (!res.ok) {
-            //     throw new Error("Something went wrong");
-            //   }
+               flag=false
+               break;
+           }
+       }
+       if(flag==true){
+           dataOTP(userObj)
+           Checkotp(OTP)
+       
 
-            return res.json()
-        })
-        .then((data)=>{
-            console.log(data)
-        })
-    }
- 
-    function dataOTP(userObj){
-        fetch(`${usersapi}`,{
-            method:"POST",
-            headers:{
-                "Content-type":"application/json",
-            },
-            body:JSON.stringify(userObj)
-        })
-        .then((res)=>{
-            return res.json()
-        })
-        .then((data)=>{
-            console.log(data)
-        })
-    }
+           id++
+           console.log(id)
+       }
+     }); 
+   }
+   })
+   function dataOtpPatch(OTP,data1){
+       fetch(`${usersapi}/${data1}`,{
+           method:"PATCH",
+           headers:{
+               "Content-type":"application/json",
+           },
+           body:JSON.stringify({otp:OTP})
+       })
+       .then((res)=>{
 
+           // if (!res.ok) {
+           //     throw new Error("Something went wrong");
+           //   }
 
+           return res.json()
+       })
+       .then((data)=>{
+           console.log(data)
+       })
+   }
+
+   function dataOTP(userObj){
+       fetch(`${usersapi}`,{
+           method:"POST",
+           headers:{
+               "Content-type":"application/json",
+           },
+           body:JSON.stringify(userObj)
+       })
+       .then((res)=>{
+           return res.json()
+       })
+       .then((data)=>{
+           console.log(data)
+       })
+   }
 
 
-    function Checkotp(OTP){
-        swal({
-            title: 'OTP FOR LOGIN',
-            text: `OTP : ${OTP}`,
-            icon: 'info',
-            showCancelButton: false,
-            confirmButtonText: 'OK'
-          }).then((result) => {
-            if (result.value) {
-              // Step 2: Prompt for OTP
-              swal({
-                title: 'Enter OTP',
-                input: 'text',
-                inputAttributes: {
-                  autocapitalize: 'off'
-                },
-                showCancelButton: true,
-                confirmButtonText: 'Submit',
-                showLoaderOnConfirm: true,
-              }).then((result) => {
-                if (result.value) {
-                  if (result.value==OTP) {
-                  
-                    swal({
-                      title: 'Logged In successfully!',
-                      text: 'You are logged in successfully.',
-                      icon: 'success'
-                    });
-                    
-                    JSON.parse(localStorage.getItem("login")) || "Login";
 
-                    localStorage.setItem("login",JSON.stringify(myForm.phone.value))
 
-                    this.location.reload()
-
-                    let loginBtn = document.getElementById("login-btn");
- 
-                   
-
-                  } else {
-
-           
-                    loginBtn.innerText='Login'
-        
-                   
+   function Checkotp(OTP){
+       swal({
+           title: 'OTP FOR LOGIN',
+           text: `OTP : ${OTP}`,
+           icon: 'info',
+           showCancelButton: false,
+           confirmButtonText: 'OK'
+         }).then((result) => {
+           if (result.value) {
+             // Step 2: Prompt for OTP
+             swal({
+               title: 'Enter OTP',
+               input: 'text',
+               inputAttributes: {
+                 autocapitalize: 'off'
+               },
+               showCancelButton: true,
+               confirmButtonText: 'Submit',
+               showLoaderOnConfirm: true,
+             }).then((result) => {
+               if (result.value) {
+                 if (result.value==OTP) {
+                 
                    swal({
+                     title: 'Logged In successfully!',
+                     text: 'You are logged in successfully.',
+                     icon: 'success'
+                   });
+                   
+                   JSON.parse(localStorage.getItem("login")) || "Login";
 
-                      title: 'Invalid OTP',
-                      text: 'Try Again',
-                      icon: 'error'
-                    });
-                  }
-                }
-              });
-            }
-          });
-    }
+                   localStorage.setItem("login",JSON.stringify(myForm.phone.value))
 
+                   this.location.reload()
 
-    let getvalue =  JSON.parse(localStorage.getItem("login")) || "Login";
+                   let loginBtn = document.getElementById("login-btn");
 
-    let loginbt = document.getElementById("login-btn")
-    
-    
-    loginbt.innerText= "Hi " + getvalue
+                  
 
+                 } else {
 
+          
+                   loginBtn.innerText='Login'
+       
+                  
+                  swal({
 
-
-
-    loginbt.parentNode.appendChild(logoutBtn);
-
-    
-    logoutBtn.addEventListener("click", function() {
-        
-        loginbt.parentNode.appendChild(logoutBtn).style.display="none";;
-    swal({
-
-        title: 'Logout Successfully',
-        text: 'Thank you for visiting.',
-        icon: 'success'
-      })
+                     title: 'Invalid OTP',
+                     text: 'Try Again',
+                     icon: 'error'
+                   });
+                 }
+               }
+             });
+           }
+         });
+   }
 
 
-  localStorage.removeItem("login");
-  loginbt.innerText = "Login";
-});
+   let getvalue =  JSON.parse(localStorage.getItem("login")) || "Login";
+
+   let loginbt = document.getElementById("login-btn")
+   
+   
+   loginbt.innerText= "Hi " + getvalue
+
+
+
+
+
+   loginbt.parentNode.appendChild(logoutBtn);
 
    
+   logoutBtn.addEventListener("click", function() {
+       
+       loginbt.parentNode.appendChild(logoutBtn).style.display="none";;
+   swal({
+
+       title: 'Logout Successfully',
+       text: 'Thank you for visiting.',
+       icon: 'success'
+     })
+
+
+ localStorage.removeItem("login");
+ loginbt.innerText = "Login";
+});
+
+  
